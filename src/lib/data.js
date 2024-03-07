f1c94dab700b7cc041bbc64f17acf1c1919cb451
jordanfeldstein <83356267+jordanfeldstein@users.noreply.github.com>
2024-03-07 07:38:07 -0500

    Update data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,95 +1,23 @@
 export const CONNECTION_GAMES = [
     [
         {
-            category: "AP Electives",
-            words: ["Stat", "Comp Sci", "Psych", "Art History"],
+            category: "Spring Sports",
+            words: ["Lacrosse", "Flag Football", "Tennis", "Baseball"],
             difficulty: 1,
         },
         {
-            category: "21st Century Classes",
-            words: ["Java", "DDP", "Robotics", "Media Broadcasting"],
+            category: "School Supplies",
+            words: ["Stapler", "Pencil", "Computer", "Paper"],
             difficulty: 2,
         },
         {
-            category: "Musically related clubs",
-            words: ["RCP", "RMB", "Jazz Project", "Choir"],
+            category: "Anything But a Backpack",
+            words: ["Car", "Microwave", "Cooler", "Printer"],
             difficulty: 3,
         },
         {
-            category: "Clubs That Meet On Wednesday",
-            words: ["OCC", "VEDDA", "Tri-M Music Honors Society", "DECA"],
-            difficulty: 4,
-        },
-    ],
-    
-    
-    [
-        {
-            category: "Grade Clash Colors",
-            words: ["Red", "Blue", "White", "Black"],
-            difficulty: 1,
-        },
-        {
-            category: "Roslyn Bagel Places",
-            words: ["Goody’s", "IBagel", "Barbara’s", "Bagel Boss"],
-            difficulty: 2,
-        },
-        {
-            category: "Department Chairs",
-            words: ["Ramonetti", "Sapir", "Windwer", "Faulkner"],
-            difficulty: 3,
-        },
-        {
-            category: "Past RTV Segments",
-            words: ["Pledge", "Trivia", "This or That", "Teacher vs Teacher"],
-            difficulty: 4,
-        },
-    ],
-
-    
-    [
-        {
-            category: "Roslyn Pizza Places",
-            words: ["Ginos", "Marinara", "Carlos", "Abeetza"],
-            difficulty: 1,
-        },
-        {
-            category: "Places To Go During Free",
-            words: ["Cafe", "Study Center", "Library", "Lounge"],
-            difficulty: 2,
-        },
-        {
-            category: "Guidance Counselors",
-            words: ["Lazzaro", "Santos", "Caruso", "Totillo"],
-            difficulty: 3,
-        },
-        {
-            category: "Teachers Names That Start with J",
-            words: ["Sorensen", "Geller", "Mumma", "Dispigno"],
-            difficulty: 4,
-        },
-    ],
-
-
-    [
-        {
-            category: "Languages Offered",
-            words: ["Spanish", "French", "Mandarin", "ASL"],
-            difficulty: 1,
-        },
-        {
-            category: "Clash Theme Days",
-            words: ["Pajamas", "Decades", "Western", "Backpack"],
-            difficulty: 2,
-        },
-        {
-            category: "Drinks Sold In The Cafe",
-            words: ["Snapple", "Poppi", "Water", "Hals"],
-            difficulty: 3,
-        },
-        {
-            category: "Departments That Don't Have an Instagram",
-            words: ["Math", "Science", "Social Studies", "Language"],
+            category: "Materials for X-Period Activities",
+            words: ["Rope", "Basketball", "Scooter", "Chair"],
             difficulty: 4,
         },
     ],
